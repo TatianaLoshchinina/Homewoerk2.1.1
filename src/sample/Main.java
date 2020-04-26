131b2a0484222852b70d08f1e76ac9643621928d
tatanalosinina <tatana@localhost>
2020-04-26 21:13:32 +0300

    first commit

---
+++ src/sample/Main.java
@@ -0,0 +1,7 @@
+public class Main{
+    public static void main(String args[]){
+        double tic =999.99;
+        int mil = (int) (tic/20);
+        System.out.println("Вам начиленно за покупку билета" +" "+ mil +" "+ "миль");
+    }
+}
